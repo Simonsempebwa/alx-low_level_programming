21c39c99929b8f0b7e72da0f1968372034052062
Simonsempebwa <chempewah19@gmail.com>
2022-10-16 03:21:17 -0700

    My stella

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,9 +10,9 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
-	{        printf("%d is a positive number\n", n);
+	{
+		printf("%d is a positive number\n", n);
 	}
 	else if (n == 0)
 	{
