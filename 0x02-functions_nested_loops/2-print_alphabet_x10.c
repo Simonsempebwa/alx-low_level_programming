037c3328f4572015ef80a753111e3f20721f9cd9
Simonsempebwa <chempewah19@gmail.com>
2022-10-19 00:49:26 -0700

    My stella

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+ /**
+  * print_alphabet_x10 -prints laphabet 10 times
+  */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char l;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (l = 'a'; l <= 'z'; l++)
+			_putchar(la);
+		_putchar('\n');
+	}
